92c7e1e43614d931cef3b8ca15abcacda21714ea
Liam Martens <hi@liammartens.com>
2025-05-26 13:20:51 -0400

    feat: Generate exports

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,4 +5,11 @@ for f in $(find ./gtfs_raw -name "*.txt"); do
   JSON_FILENAME="./gtfs_json/${FILE_DIRNAME}/${FILE_BASENAME%.txt}.json"
   mkdir -p $(dirname $JSON_FILENAME)
   npm exec csv2json -- -d $f $JSON_FILENAME
+
+  # add to JSON exports
+  PKGJSON_EXPORTS_NAME="./gtfs/$FILE_DIRNAME/${FILE_BASENAME%.txt}.json"
+  PKGJSON_EXPORTS_TARGET="./gtfs_json/${FILE_DIRNAME}/${FILE_BASENAME%.txt}.json"
+  jq --arg PKGJSON_EXPORTS_NAME $PKGJSON_EXPORTS_NAME --arg PKGJSON_EXPORTS_TARGET $PKGJSON_EXPORTS_TARGET \
+    '.exports[$PKGJSON_EXPORTS_NAME] = $PKGJSON_EXPORTS_TARGET' \
+    ./package.json > ./package.json.tmp && mv ./package.json.tmp ./package.json
 done
