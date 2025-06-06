29877292d8f990ed3e72f162adeaa01a31e522d8
Liam Martens <hi@liammartens.com>
2025-06-06 10:36:52 -0400

    feat: use npx

--- scripts/decompress.mjs
+++
@@ -1,7 +0,0 @@
-import fs from "fs-extra";
-import zip from "cross-zip";
-
-if (fs.existsSync("./gtfs_js.zip") && fs.existsSync("./gtfs_json.zip")) {
-  zip.unzipSync("./gtfs_js.zip", "./gtfs_js");
-  zip.unzipSync("./gtfs_json.zip", "./gtfs_json");
-}
